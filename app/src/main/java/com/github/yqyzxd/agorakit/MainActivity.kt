613e021e6b4e59a4224eb5e502679b3ad28e0c95
windq <shihaowind@163.com>
2022-11-22 17:45:18 +0800

    main增加agorakit

--- app/src/main/java/com/github/yqyzxd/agorakit/MainActivity.kt
+++ app/src/main/java/com/github/yqyzxd/agorakit/MainActivity.kt
@@ -2,10 +2,12 @@ package com.github.yqyzxd.agorakit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wind.agorakit.Agora
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Agora.getInstance().create(this)
     }
 }
